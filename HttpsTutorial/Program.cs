1b8fdfecb47ce032c14e8014ed71e5ea239ddf4e
IsayahC <isayahculbertson@gmail.com>
2022-04-23 10:25:57 -0400

    Add project files.

---
+++ HttpsTutorial/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Net;
+using System.Text;
+
+namespace Examples.System.Net
+{
+    public class WebRequestGetExample
+    {
+        public static void Main()
+        {
+            // Create a request for the URL. 		
+            //WebRequest request = WebRequest.Create("http://www.contoso.com/default.html");
+            //// If required by the server, set the credentials.
+            //request.Credentials = CredentialCache.DefaultCredentials;
+            //// Get the response.
+            //HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            //// Display the status.
+            //Console.WriteLine(response.StatusDescription);
+            //// Get the stream containing content returned by the server.
+            //Stream dataStream = response.GetResponseStream();
+            //// Open the stream using a StreamReader for easy access.
+            //StreamReader reader = new StreamReader(dataStream);
+            //// Read the content.
+            //string responseFromServer = reader.ReadToEnd();
+            //// Display the content.
+            //Console.WriteLine(responseFromServer);
+            //// Cleanup the streams and the response.
+            //reader.Close();
+            //dataStream.Close();
+            //response.Close();
+
+            string[] test = { "http://contoso.com:8080/index/" };
+            SimpleListenerExample(test);
+        }
+
+        public static void NonblockingListener(string[] prefixes)
+        {
+            HttpListener listener = new HttpListener();
+            foreach (string s in prefixes)
+            {
+                listener.Prefixes.Add(s);
+            }
+            listener.Start();
+            IAsyncResult result = listener.BeginGetContext(new AsyncCallback(ListenerCallback), listener);
+            // Applications can do some work here while waiting for the
+            // request. If no work can be done until you have processed a request,
+            // use a wait handle to prevent this thread from terminating
+            // while the asynchronous operation completes.
+            Console.WriteLine("Waiting for request to be processed asyncronously.");
+            result.AsyncWaitHandle.WaitOne();
+            Console.WriteLine("Request processed asyncronously.");
+            listener.Close();
+        }
+
+        public static void ListenerCallback(IAsyncResult result)
+        {
+            HttpListener listener = (HttpListener)result.AsyncState;
+            // Call EndGetContext to complete the asynchronous operation.
+            HttpListenerContext context = listener.EndGetContext(result);
+            HttpListenerRequest request = context.Request;
+            // Obtain a response object.
+            HttpListenerResponse response = context.Response;
+            // Construct a response.
+            string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
+            byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+            // Get a response stream and write the response to it.
+            response.ContentLength64 = buffer.Length;
+            Stream output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+            // You must close the output stream.
+            output.Close();
+        }
+
+        // This example requires the System and System.Net namespaces.
+        public static void SimpleListenerExample(string[] prefixes)
+        {
+            if (!HttpListener.IsSupported)
+            {
+                Console.WriteLine("Windows XP SP2 or Server 2003 is required to use the HttpListener class.");
+                return;
+            }
+            // URI prefixes are required,
+            // for example "http://contoso.com:8080/index/".
+            if (prefixes == null || prefixes.Length == 0)
+                throw new ArgumentException("prefixes");
+
+            // Create a listener.
+            HttpListener listener = new HttpListener();
+            // Add the prefixes.
+            foreach (string s in prefixes)
+            {
+                listener.Prefixes.Add(s);
+            }
+            listener.Start();
+            Console.WriteLine("Listening...");
+            // Note: The GetContext method blocks while waiting for a request.
+            HttpListenerContext context = listener.GetContext();
+            HttpListenerRequest request = context.Request;
+            // Obtain a response object.
+            HttpListenerResponse response = context.Response;
+            // Construct a response.
+            string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
+            byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+            // Get a response stream and write the response to it.
+            response.ContentLength64 = buffer.Length;
+            Stream output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+            // You must close the output stream.
+            output.Close();
+            listener.Stop();
+        }
+    }
+}
